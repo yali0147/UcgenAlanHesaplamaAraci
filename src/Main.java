33116b3b342544187530cc9a4bc928194f54e3ed
yali <yusufali0147@hotmail.com>
2023-08-10 19:21:41 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Üçgen Alan Hesaplama Aracı");
+
+
+        double a , b ,c ,d ,u ;
+
+        Scanner inp =new Scanner(System.in);
+
+        System.out.print("1. kenarı giriniz : ");
+        a = inp.nextDouble();
+        System.out.print("2. kenarı giriniz : ");
+        b = inp.nextDouble();
+        System.out.print("3. kenarı giriniz : ");
+        c = inp.nextDouble();
+
+        u= (a+b+c)/2;
+        d= Math.sqrt(u*(u-a)*(u-b)*(u-c));
+
+        System.out.println("Üçgenin Alanı : " +d );
+
+
+
+
+
+
+
+
     }
 }
